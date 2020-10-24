59f5c0c78d4ab04a7eef9455442b7668065709ee
alias-rahil <rahil.kabani.4@gmail.com>
2020-10-25 04:46:57 +0530

    build image before docker-compose

--- .github/release/release.sh
+++ .github/release/release.sh
@@ -1,2 +1,4 @@
+docker build . -t evilfreelancer-docker-lfs-build
 cp docker-compose.yml.dist docker-compose.yml
+sed -i 's|evilfreelancer/docker-lfs-build|evilfreelancer-docker-lfs-build|g' docker-compose.yml
 docker-compose up
