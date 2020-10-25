28f3a1f49b1de27916cff25049da757f38aa78da
alias-rahil <rahil.kabani.4@gmail.com>
2020-10-25 15:31:58 +0530

    fix entrypoint in docker

--- .github/release/release.sh
+++ .github/release/release.sh
@@ -1,4 +1,5 @@
 docker build . -t evilfreelancer-docker-lfs-build
 cp docker-compose.yml.dist docker-compose.yml
 sed -i 's|evilfreelancer/docker-lfs-build|evilfreelancer-docker-lfs-build|g' docker-compose.yml
+sed -i 's|["sleep", "inf"]|["bash", "/book/book.sh"]|g' docker-compose.yml
 docker-compose up
